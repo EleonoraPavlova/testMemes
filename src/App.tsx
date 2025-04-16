72213cf0e443cdf9ceade5e67bf0b967c6cbc0f9
eleonora <nebrudko18@gmail.com>
2025-04-16 17:15:16 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Button} from "@heroui/react";
+import './App.css'
+
+function App() {
+ return (
+  <>
+   <h1 className="text-3xl font-bold underline">
+    Hello world!
+   </h1>
+   <Button>Press me</Button>
+  </>
+ )
+}
+
+export default App
